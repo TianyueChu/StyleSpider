243755966829c3f0f84e5f92261595dfa0fc40b5
Tianyue Chu <30310264+TianyueChu@users.noreply.github.com>
2024-04-16 12:09:48 +0200

    Update category.py

--- category.py
+++ category.py
@@ -23,4 +23,4 @@ if __name__ == "__main__":
     baseurl = config.baseurl
     category_list = categorylist(baseurl)
     print("category list", category_list)
-    print("end point", category_list.index('https://www.zara.com/es/en/woman-shoes-l1251.html'))
+    print("end point", category_list.index(url))
